cdb70f0831bfd014c32bd56f031ec1df95b44ba2
SaltzmanAlaric <SaltzmanAlaric@github.com>
2019-12-23 10:56:32 +0800

    高度显示问题

--- js/archive.js
+++ js/archive.js
@@ -26,7 +26,7 @@ const fillHTML = function (data) {
         strHtml += "<div class='history-date'><ul>";
         strHtml += "<h2 class='" + (i === 0 ? "first" : "date02") + "'><a>" + item.year + "年</a>" + getLabelCnt(item.labelMap) + "</h2>";
         $.each(item.acts, function (j, n) {
-            strHtml += "<li class='" + ((j === 0 || n.isBig) ? "green" : "") + "'><h3>" + n.date + "<span>" + item.year + "</span></h3><dl><dt>" + n.title + (i+j === 0 ? "<span style='display: inline-block;vertical-align: super;color:#f00;font-size:10px;'>新</span>" : "") + "<a target='_blank' href='" + n.href + "'>⇲</a><span>" + n.desc + "</span></dt></dl></li>";
+            strHtml += "<li class='" + (j === 0 ? "green" : "") + "'><h3>" + n.date + "<span>" + item.year + "</span></h3><dl><dt>" + n.title + (i+j === 0 ? "<span style='display: inline-block;vertical-align: super;color:#f00;font-size:10px;'>新</span>" : "") + "<a target='_blank' href='" + n.href + "'>⇲</a><span>" + n.desc + "</span></dt></dl></li>";
         }); //end inner-each
         strHtml += "</ul></div>";
     }); // end outer-each
@@ -39,7 +39,7 @@ const systole = function () {
     }
     let $wrapEle = $(".history-date");
     let $targetA = $wrapEle.find("h2 a");
-    let parentH = $wrapEle.parent().height();
+    let parentH = $wrapEle.parent().height()+100;
     $wrapEle.parent().css({"height": 59});
     let eleTop = [];
     setTimeout(function () {
